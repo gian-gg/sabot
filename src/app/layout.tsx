ec6dd03a4be9842c8215c273db803c6ee15f026a
gian.gg <gianepanto@gmail.com>
2025-10-03 02:31:53 +0800

    refactor: prettier format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "jiji",
-  description: "Yet another finance app, this time with AI.",
+  title: 'jiji',
+  description: 'Yet another finance app, this time with AI.',
 };
 
 export default function RootLayout({
