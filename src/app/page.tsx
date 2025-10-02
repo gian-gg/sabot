b8a2c74bcdd49689be9af8331e00b8be4d2a902b
gian.gg <gianepanto@gmail.com>
2025-10-03 02:47:04 +0800

    feat: init shadcn, add login form, and wallet route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
   return (
-    <div className="rounded-lg bg-blue-500 p-4 text-white">Hello World</div>
-  );
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc.
+        </a>
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
---
+++ src/app/wallet/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      Hello, World!
+    </div>
+  );
+}
