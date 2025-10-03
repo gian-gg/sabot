98405365b524361e38be6c1e7deb7c0ed57d9561
gian.gg <gianepanto@gmail.com>
2025-10-03 14:12:36 +0800

    feat: login ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'sonner';
 import type { Metadata } from 'next';
 import './globals.css';
 
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        {children}
+        <Toaster richColors />
+      </body>
     </html>
   );
 }
