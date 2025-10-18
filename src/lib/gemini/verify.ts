ee482f4e4b0835048ff523030384773cc4e6c782
gian.gg <gianepanto@gmail.com>
2025-10-19 06:07:33 +0800

    refactor(gemini): add reusable client instance for centralized setup

--- src/lib/gemini/verify.ts
+++ src/lib/gemini/verify.ts
@@ -1,22 +1,16 @@
 'use server';
 
-import { GoogleGenAI, Part } from '@google/genai';
+import geminiClient from '@/lib/gemini';
+import { Part } from '@google/genai';
 import type { GovernmentIdInfo, IdType } from '@/types/verify';
 
 import { idExtractionPromptTemplate, geminiModel } from '@/constants/gemini';
-
-const API_KEY = process.env.GEMINI_API_KEY;
-if (!API_KEY) {
-  throw new Error(
-    'GEMINI_API_KEY environment variable is not set on the server.'
-  );
-}
-const ai = new GoogleGenAI({}); // Gemini client instance
 
 export async function extractID(
   idType: IdType,
   file: File
 ): Promise<GovernmentIdInfo> {
+  const ai = geminiClient();
   // Read the uploaded file into a base64 string
   const buffer = await file.arrayBuffer();
   const base64Data = Buffer.from(buffer).toString('base64');
