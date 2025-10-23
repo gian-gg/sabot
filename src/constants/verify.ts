933dd77bf71d61d001ff4d0220c6790ffcbeb517
gian.gg <gianepanto@gmail.com>
2025-10-23 18:49:22 +0800

    fix: blink to close eyes

--- src/constants/verify.ts
+++ src/constants/verify.ts
@@ -20,17 +20,13 @@ export const LIVENESS_CHECK_STEPS_FIXED = [
   'Look straight ahead',
   'Turn your head to the left',
   'Turn your head to the right',
-  // 'Smile',
-  // 'Blink your eyes',
-  // 'Open your mouth',
-  // 'Raise your eyebrows',
 ];
 
 export const LIVENESS_CHECK_STEPS_RANDOM = [
   'Smile',
-  'Blink your eyes',
   'Open your mouth',
   'Raise your eyebrows',
+  'Close your eyes',
 ];
 
 export const LIVENESS_CHECK_MAX_STEPS = 4;
