d9097fc465b60e572e9dfb363de56d0e4716a52a
gian.gg <gianepanto@gmail.com>
2025-10-23 20:12:08 +0800

    feat: add pending verificationStatus state to components

--- src/components/home/hero-action.tsx
+++ src/components/home/hero-action.tsx
@@ -2,15 +2,21 @@
 
 import React from 'react';
 import { Button } from '../ui/button';
-import { Plus, BadgeCheck } from 'lucide-react';
+import { Plus, BadgeCheck, Clock } from 'lucide-react';
 import Link from 'next/link';
 import { ROUTES } from '@/constants/routes';
 import { useUserStore } from '@/store/user/userStore';
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
 const HeroAction = () => {
   const user = useUserStore();
 
-  if (!user.isVerified) {
+  if (user.verificationStatus === 'not-started') {
     return (
       <Button asChild>
         <Link href={ROUTES.HOME.VERIFY}>
@@ -20,6 +26,31 @@ const HeroAction = () => {
       </Button>
     );
   }
+
+  if (user.verificationStatus === 'pending') {
+    return (
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button>
+            <Clock className="mr-2 h-4 w-4" />
+            Pending Verification
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent align="center" side="right">
+          <p>Verifying account, kindly check again later.</p>
+        </TooltipContent>
+      </Tooltip>
+    );
+  }
+
+  return (
+    <Button asChild>
+      <Link href={ROUTES.TRANSACTION.NEW}>
+        <Plus className="mr-2 h-4 w-4" />
+        Create Transaction
+      </Link>
+    </Button>
+  );
 };
 
 export default HeroAction;
