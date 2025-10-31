90c61866ae3f372eacccbf5e34e92dfcef09220b
eli <elipalacaraz@gmail.com>
2025-10-31 22:49:56 +0800

    fix: duplicate transaction submit in the blockchain

--- src/lib/blockchain/writeFunctions.ts
+++ src/lib/blockchain/writeFunctions.ts
@@ -5,7 +5,6 @@ import {
 import { createClient } from '@/lib/supabase/server';
 import {
   getAllUserIds,
-  getEncryptedKey,
   getPublicAddress,
   getTransactionDetails,
   postHashTransaction,
@@ -100,6 +99,27 @@ export async function pushTransactionToBlockchain(
       return false;
     }
 
+    const supabase = await createClient();
+
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
+
+    if (error || !user) {
+      console.error('registerUser: Failed to get user:', error?.message);
+      return false;
+    }
+
+    const userId = user.id;
+
+    if (userId !== creatorUser.user_id) {
+      console.log(
+        `User ${userId} is not the creator of the transaction ${transactionId}, to block duplicate push`
+      );
+      return false;
+    }
+
     const contract = await hostWritableLedgerContract();
 
     if (!contract) {
@@ -109,7 +129,6 @@ export async function pushTransactionToBlockchain(
       return false;
     }
 
-    // Step 5: Push to blockchain
     console.log(
       `pushTransactionToBlockchain: Sending transaction to blockchain...`
     );
